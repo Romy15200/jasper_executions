85bf69f374ede1f34ab14cd12ee2fc89b9070adc
Romy Peled <romy.peled@campus.technion.ac.il>
2025-11-19 09:48:06 +0200

    fixed sv files

---
+++ liveness/1_delay_10.sv
@@ -0,0 +1,13 @@
+module DELAY (input clk, input rst, output reg sig ,output reg err, output reg flg);
+  localparam N = 20000;
+  localparam CBITS = 15;
+  reg [CBITS-1 :0] cnt;
+  assign sig = (cnt >= N);
+  assign err = (cnt > N);
+  assign flg = (cnt < N);
+  always @(posedge clk) begin
+    if (rst || cnt >= N) cnt <= 0;
+    else cnt <= cnt + 1; 
+  end
+	p1: assert property  ((@(posedge clk) s_eventually !rst -> sig)) ;
+endmodule
---
+++ safety/s_gray_3.sv
@@ -0,0 +1,14 @@
+module GRAY #(localparam CBITS = 10) (input clk, input rst, output reg [CBITS-1:0] gray_c, output reg sig, output reg flg);
+  reg [CBITS-1:0] cnt;
+  assign sig = (cnt == 0) & ~rst;
+  assign flg = (cnt >= 0);
+  always@(posedge clk, posedge rst) begin
+    if (rst)
+      cnt <= 0;
+    else begin
+      cnt <= cnt + 1;
+      gray_c = (cnt) ^ ((cnt) >> 1);
+    end
+  end
+	p1: assert property  ((@(posedge clk) sig && !rst |=> !sig)) ;
+endmodule
