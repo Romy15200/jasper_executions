88958cb368946a9b232d287d742f88e5fc3258d9
Ykir- Romy Peled <rompel@vlsi-ria85.technion.ac.il>
2025-11-27 22:21:41 +0200

    PWM outputs

---
+++ safety_liveness/jgprojects/1_PWM_11/sessionLogs/session_0/jg_session_0.cmd
@@ -0,0 +1,17 @@
+# ----------------------------------------
+# Jasper Version Info
+# tool      : Jasper 2024.06
+# platform  : Linux 3.10.0-1062.el7.x86_64
+# version   : 2024.06p002 64 bits
+# build date: 2024.09.02 16:28:38 UTC
+# ----------------------------------------
+# started   : 2025-11-27 12:56:25 IST
+# hostname  : vlsi-moon.vlsidomain
+# pid       : 50086
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:37690' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AAAAznicY2RgYLCp////PwMYMD6A0Aw2jAyoAMRnQhUJbEChGRhYYZphSliAmItBl6GAoYghnyELyNNh0ANCfSA7HS6aypDMUMJQDBQ1ZIhnCGAIZ/AF0oZAyMDAA9SdxJAIlE9myADyJTDU6IHlcsD2AQA0xBYl' '-proj' '/users/rompel/jasper_executions/safety_liveness/jgprojects/1_PWM_11/sessionLogs/session_0' '-init' '-hidden' '/users/rompel/jasper_executions/safety_liveness/jgprojects/1_PWM_11/.tmp/.initCmds.tcl' '1_PWM_11.tcl' '-hidden' '/users/rompel/jasper_executions/safety_liveness/jgprojects/1_PWM_11/.tmp/.postCmds.tcl'
+analyze -sv09 ../1_PWM_11.sv
+elaborate -bbox_mul 1000000 -bbox_div 1000000 -bbox_mod 1000000 -bbox_a 1000000
+reset -none
+clock clk
+prove -all
+exit -force
---
+++ safety_liveness/jgprojects/1_PWM_5/sessionLogs/session_0/jg_session_0.cmd
@@ -0,0 +1,17 @@
+# ----------------------------------------
+# Jasper Version Info
+# tool      : Jasper 2024.06
+# platform  : Linux 3.10.0-1062.el7.x86_64
+# version   : 2024.06p002 64 bits
+# build date: 2024.09.02 16:28:38 UTC
+# ----------------------------------------
+# started   : 2025-11-27 19:24:13 IST
+# hostname  : vlsi-moon.vlsidomain
+# pid       : 242890
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:36929' '-nowindow' '-style' 'windows' '-exitonerror' '-data' 'AAAAynicY2RgYLCp////PwMYMD6A0Aw2jAyoAMRnQhUJbEChGRhYYZphSliAmItBl6GAoYghnyELyNNi0ANCfSA7HS6aypDMUMJQDBQ1ZIhnCGAIZ/AF0qZA1TxAvUkMiUDZZIYMIF8MTYUeWCYHbBcA4AcVxw==' '-proj' '/users/rompel/jasper_executions/safety_liveness/jgprojects/1_PWM_5/sessionLogs/session_0' '-init' '-hidden' '/users/rompel/jasper_executions/safety_liveness/jgprojects/1_PWM_5/.tmp/.initCmds.tcl' '1_PWM_5.tcl' '-hidden' '/users/rompel/jasper_executions/safety_liveness/jgprojects/1_PWM_5/.tmp/.postCmds.tcl'
+analyze -sv09 ../1_PWM_5.sv
+elaborate -bbox_mul 1000000 -bbox_div 1000000 -bbox_mod 1000000 -bbox_a 1000000
+reset -none
+clock clk
+prove -all
+exit -force
